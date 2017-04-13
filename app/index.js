94c6df673f00358d34f6cd1cac31299e44591b09
shitala-cuelogic <shital.agarwal@cuelogic.co.in>
2017-04-13 16:37:53 +0530

    Hello world example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log("App start");
+import React from "react";
+import {render} from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello All
+      </div>
+    );
+  }
+}
+
+
+render(<App/>, window.document.getElementById("app"));
